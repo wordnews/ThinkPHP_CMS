33e3f1a6249fdaed8da6fde814bf83f97ec35363
刘俊龙 <a1018808441@gmail.com>
2016-08-03 22:40:11 +0800

    更新代码

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -2,8 +2,8 @@
 return array(
 	//'配置项'=>'配置值'
     'DB_TYPE'               =>  'mysql',     // 数据库类型
-    'DB_HOST'               =>  'localhost', // 服务器地址
-    'DB_NAME'               =>  'demo',          // 数据库名
+    'DB_HOST'               =>  '127.0.0.1', // 服务器地址
+    'DB_NAME'               =>  'ThinkPHP_CMS',          // 数据库名
     'DB_USER'               =>  'root',      // 用户名
     'DB_PWD'                =>  '',          // 密码
     'DB_PORT'               =>  '3306',        // 端口
